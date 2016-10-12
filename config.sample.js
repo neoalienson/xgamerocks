1795ad162d7472a29c7d5a79eb473791c00f7c44
Neo Alienson <neo@orz.hk>
2016-10-12 16:53:51 -0400

    new option to skip the sms verificaiton

--- config.sample.js
+++ config.sample.js
@@ -1,5 +1,6 @@
 module.exports = {
   authyApiKey: 'YOUR AUTHY API KEY',
+  skipSms: true,
   port: process.env.PORT || 8000,
   parseAppId: "YOUR_APP_ID",
   parseUrl: 'http://YOUR_PARSE_SERVER:1337/parse',
